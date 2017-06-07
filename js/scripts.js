baff2a545ed98c3a8e82c078c8a50a6caa8879cc
Guest User <Guest@epicodus-2.local>
2017-06-07 08:30:54 -0700

    seperate business & UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
-$(document).ready(function() {
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+  $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
-  });
-  var leapYear = function(year) {
-    if (year % 4 === 0) {
-      return true;
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
     } else {
-      return false;
+      $(".not").text("");
     }
-  };
+
+    $("#result").show();
+  });
 });
